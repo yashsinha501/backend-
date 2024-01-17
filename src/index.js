62b3879b632da05e369f216284c9f626119b9117
yashsinha501 <72601810+yashsinha501@users.noreply.github.com>
2024-01-18 03:37:10 +0530

    DB Connection

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+// import mongoose from "moongose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+// const app = express()
+
+
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error",()=>{
+//             console.log("ERR: ",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR: ",error)
+//         throw err
+//     }
+// })()
