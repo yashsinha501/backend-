f9dd25c9b09bc5a98eeaf209075d4687a66203b9
yashsinha501 <72601810+yashsinha501@users.noreply.github.com>
2024-01-18 04:11:03 +0530

    error handling

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusCode, data, manage = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = this.message
+        this.success = statusCode < 400
+    }
+}
