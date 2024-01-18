cabf5cb5c864a5d24a197dc3af277995ee61bafd
yashsinha501 <72601810+yashsinha501@users.noreply.github.com>
2024-01-18 13:53:54 +0530

    register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose, {Schema} from "moongose";
+import mongoose, {Schema} from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
 
@@ -45,7 +45,7 @@ const userSchema = new Schema(
 userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next();
 
-    this.password=bcrypt.hash(this.password, 10)
+    this.password= await bcrypt.hash(this.password, 10)
     next()
 })
 
